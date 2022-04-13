46711e5ae3a4b0c58262929cc97a65eae3557f74
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2022-04-13 23:48:02 +0900

    Refactor split function

--- main.dart
+++ main.dart
@@ -5,13 +5,17 @@ import 'package:shelf/shelf_io.dart' as io;
 void main() async {
   var app = Router();
 
-  app.get('/hello', (Request request) {
-    return Response.ok('hello-world');
-  });
+  app.get('/hello', hello);
 
-  app.get('/user/<user>', (Request request, String user) {
-    return Response.ok('hello $user');
-  });
+  app.get('/user/<user>', user);
 
   var server = await io.serve(app, 'localhost', 8080);
+}
+
+Response hello(Request request) {
+  return Response.ok('hello-world');
+}
+
+Response user(Request request, String user) {
+  return Response.ok('hello $user');
 }
