a74490b95f00aacf297c0c1cdbd6efa4488cb6ee
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2022-04-13 23:44:10 +0900

    Feat api server

--- main.dart
+++ main.dart
@@ -1,9 +1,17 @@
-void printInteger(int num) {
-  print("the number is $num");
-}
+import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
 
-void main() {
-  var num = 50;
-  printInteger(num);
-  assert(false);
+void main() async {
+  var app = Router();
+
+  app.get('/hello', (Request request) {
+    return Response.ok('hello-world');
+  });
+
+  app.get('/user/<user>', (Request request, String user) {
+    return Response.ok('hello $user');
+  });
+
+  var server = await io.serve(app, 'localhost', 8080);
 }
